5aba77e6abac95bbe66eaeafd9f5977fb0823deb
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-08-08 10:49:16 -0300

    feat: Mapa renderizado de acordo com a localização do usuário

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+      console.log(`https://www.google.com.br/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView(coords, 14);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    () => alert('Não conseguimos acessar sua localização.')
+  );
+}
